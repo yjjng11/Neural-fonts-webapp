f6570b500df7097d1e0c9132aea905984adc76b6
hjj <hjj@altair.snu.ac.kr>
2017-08-02 19:01:11 +0900

    Introdution + Input template + exmaple page

--- routes/index.js
+++ routes/index.js
@@ -48,6 +48,14 @@ router.get('/adjust', function(req, res, next){
   res.render('adjust');
 });
 
+router.get('/examples', function(req, res, next){
+  res.render('examples');
+});
+
+router.get('/download_template', function(req, res){
+  var file = __dirname + '/../public/images/Random_template.pdf';
+  res.download(file);
+});
 
 var count = 0;
 router.post('/progress',function(req,res){
