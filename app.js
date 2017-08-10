84c7af071f2756586b1aa08e75336770fbf7f48e
Yejin-PC\Yejin <jyjng01@gmail.com>
2017-08-10 17:15:40 +0900

    delete comment

--- app.js
+++ app.js
@@ -45,45 +45,5 @@ app.use(function(err, req, res, next) {
 app.listen(3000,function(){
   console.log("server on!");
 });
-/*
-var fs = require('fs');
 
-var ImageTracer = require( __dirname + '/imagetracer_v1.2.1' );
-
-// This example uses https://github.com/arian/pngjs
-// , but other libraries can be used to load an image file to an ImageData object.
-var PNGReader = require( __dirname + '/PNGReader' );
-
-fs.readFile(
-
-	__dirname + '/public/images/bb.png', // Input file path
-
-	function( err, bytes ){
-		if(err){ throw err; }
-
-		var reader = new PNGReader(bytes);
-
-		reader.parse( function( err, png ){
-			if(err){ throw err; }
-
-			// creating an ImageData object
-			var myImageData = { width:png.width, height:png.height, data:png.pixels };
-
-			// tracing to SVG string
-			var options = {  }; // optional
-			var svgstring = ImageTracer.imagedataToSVG( myImageData, options );
-
-			// writing to file
-			fs.writeFile(
-				__dirname + '/public/images/bb.svg', // Output file path
-				svgstring,
-				function(err){ if(err){ throw err; } console.log( __dirname + '/test.svg was saved!' ); }
-			);
-
-		});// End of reader.parse()
-
-	}// End of readFile callback()
-
-);// End of fs.readFile()
-*/
 module.exports = app;
