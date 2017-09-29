892270b26df8809053016815f0755c3147d177cd
hjj <hjj@altair.snu.ac.kr>
2017-09-29 16:28:00 +0900

    install script added

---
+++ install-pretrained-model.sh
@@ -0,0 +1,12 @@
+# download pre-trained model from our one drive
+wget -O ./pre-trained.tgz "https://mysnu-my.sharepoint.com/personal/yu65789_seoul_ac_kr/_layouts/15/guestaccess.aspx?docid=0a7fcfabb78af4958b790b98eccac135c&authkey=AVqeaI5jyQHWyklZgotc04Y"
+
+# unzip the model
+tar -zxf pre-trained.tgz
+
+# place the model in the right place
+mkdir neural-fonts/binary
+mv baseline neural-fonts/binary/.
+
+# remove the model
+rm pre-trained.tgz
