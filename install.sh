93ebf7aa1fe529d0ee5070fb8db2be8f8a987888
hjj <hjj@altair.snu.ac.kr>
2017-10-23 18:20:11 +0900

    Fixed a bug in install process

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+# copy the right js file for generating Korean svg font
+cp public/javascripts/os2.js node_modules/svg2ttf/lib/ttf/tables/.
+
 # download pre-trained model from our one drive
 wget -O ./pre-trained.tgz "https://mysnu-my.sharepoint.com/personal/yu65789_seoul_ac_kr/_layouts/15/guestaccess.aspx?docid=0a7fcfabb78af4958b790b98eccac135c&authkey=AVqeaI5jyQHWyklZgotc04Y"
 
